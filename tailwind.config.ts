68aeb8fa80d0126212f2374be614c8beb8713ad3
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-09-18 16:51:46 -0300

    adição do  tema

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'pink': '#C77373',
+      'green': '#4AA5A1',
+      'gray-dark': '#2D3748',
+      'black': '#050F12',
+      'white': '#FFFFFF'
     },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      serif: ['Montserrat', 'serif'],
+    }
   },
   plugins: [],
 }
