6be52e654870d4932e89f9b0ab4b51c5a5cbdfcb
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-09-22 16:52:06 -0300

    responsividade tecnologias

--- src/sections/technologies/TechList.tsx
+++ src/sections/technologies/TechList.tsx
@@ -9,10 +9,10 @@ const TechList = ({ list, area, color }: TechListProps) => {
 
   return (
     <article
-      className={`flex flex-col items-center p-12 w-96 h-80 shadow-md ${color}`}
+      className={`flex flex-col items-center p-12 md:w-96 h-72 lg:h-80 xl:w-1/3 shadow-md ${color}`}
     >
-      <p className="font-bold text-4xl tracking-wide pb-8 ">{area}</p>
-      <p className="text-xl text-center tracking-wide">{formattedList}</p>
+      <p className="font-bold text-3xl md:text-4xl tracking-wide pb-8 ">{area}</p>
+      <p className="text-xl xl:text-2xl text-center tracking-wide">{formattedList}</p>
     </article>
   );
 };
--- src/sections/technologies/index.tsx
+++ src/sections/technologies/index.tsx
@@ -3,7 +3,7 @@ import { backendTechs, frontendTechs } from "./tecnologies";
 
 const Technologies = () => {
   return (
-    <section className="flex w-full justify-center gap-8 p-8 relative">
+    <section className="w-full flex flex-col md:flex-row justify-center gap-8 md:justify-around p-8 relative">
       <TechList area="Frontend" color="bg-cyan" list={frontendTechs} />
       <TechList area="Backend" color="bg-gray-dark" list={backendTechs} />
     </section>
